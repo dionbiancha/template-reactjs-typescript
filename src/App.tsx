2a1dfd683f8424347ec9e7866265ae98554c1852
Dionei Bianchati <dion.biancha@gmail.com>
2021-05-05 10:39:46 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Teste v.1
         </p>
         <a
           className="App-link"
